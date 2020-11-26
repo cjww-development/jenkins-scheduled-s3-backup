12be59c53ccd981e3c6fa71b04d2a71bfb4f0e91
chrisjwwalker <8091191+chrisjwwalker@users.noreply.github.com>
2020-11-26 20:26:42 +0000

    Added exclude to tar

--- archiving/archive.sh
+++ archiving/archive.sh
@@ -1,2 +1,3 @@
 touch ./jenkins_backup.tar
+ls -al
 tar --exclude=jenkins_backup.tar -cvf ./jenkins_backup.tar -C $JENKINS_HOME .
--- archiving/versioned-archive.sh
+++ archiving/versioned-archive.sh
@@ -1,3 +1,4 @@
 timestamp=$(date +%Y-%m-%dT%H:%M:%S)
 touch ./$timestamp.tar
+ls -al
 tar --exclude=$timestamp.tar -cvf ./$timestamp.tar -C $JENKINS_HOME .
